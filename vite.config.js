63184d5121780ffe9f4b8aa864216ead2d2ea039
Felipe Zuntini <felipezuntini@hotmail.com>
2022-05-19 19:35:04 -0300

    feat(devops): sh script for quicker deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => {
-    return {
-        port: 4080,
-        root: path.resolve(process.cwd(), './app'),
-        base: command === 'serve' ? '/' : 'https://tweenn.github.io/learning-text-to-speech/',
-    }
+	return {
+		port: 4080,
+		root: path.resolve(process.cwd(), './app'),
+		base: command === 'serve' ? '/' : 'https://tweenn.github.io/learning-text-to-speech/',
+		build: {
+			minify: 'esbuild',
+			outDir: path.resolve(process.cwd(), './dist'),
+		}
+	}
 });
