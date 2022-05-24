75c5c3fe65e406753f03bb753711ffb3888af5a0
Felipe Zuntini <felipezuntini@hotmail.com>
2022-05-24 09:50:51 -0300

    fix(vite-config): port value now works as intended

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => {
 	return {
-		port: 4080,
+		server: {
+			port: 4080,
+		},
 		root: path.resolve(process.cwd(), './app'),
 		base: command === 'serve' ? '/' : 'https://tweenn.github.io/learning-text-to-speech/',
 		build: {
